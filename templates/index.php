70579c5d052614bd60c604d859ff43df7169f717
Bjorn Kristinsson <bjornkri@gmail.com>
2011-04-16 10:54:00 -0500

    Added initial php index file

---
+++ templates/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bj&ouml;rn's saga</title>
+</head>
+<body>
+	<div id="header">
+		<h1>Bj&ouml;rn's saga</h1>
+	</div>
+	<div id="content">
+		<?php
+		if ($_GET['year'] && $_GET['month'] && $_GET['day'] && $_GET['slug']) {
+			$filename = $_GET['year'] . $_GET['month'] . $_GET['day'] . '_' . $_GET['slug'] . '.php';
+			if (file_exists($filename)){
+				include($filename);
+			}
+			else {
+				include('404.php');
+			}
+		}
+		elseif ($_GET['year'] && $_GET['month']) {
+			$filename = $_GET['year'] . $_GET['month'] . 'archive.php';
+			if (file_exists($filename)){
+				include($filename);
+			}
+			else {
+				include('404.php');
+			}
+		}
+		else {
+			include('main.php');
+		}
+		?>
+	</div>
+	<div id="sidebar">
+		
+	</div>
+</body>
+</html>
