82e1331fdda3b7d4f00621faafc4277aa6206266
Bjorn Kristinsson <bjornkri@gmail.com>
2011-04-28 02:56:08 -0500

    added draft capabilities

--- brew.py
+++ brew.py
@@ -7,10 +7,51 @@ import settings
 
 
 def main():
+    blogposts, blog_index = get_blogposts(settings.SOURCES['published'])
+    drafts, draft_index = get_blogposts(settings.SOURCES['drafts'])
+    
+    index_file = ""
+    blogs = sorted(blogposts, key=lambda blog: blog.meta['pubdate'], reverse=True)
+    
+    for blogpost in blogs:
+        filename = "%s%s%02d%02d_%s.php" % (
+                settings.SERVER_ROOT,
+                blogpost.meta['pubdate'].year, 
+                blogpost.meta['pubdate'].month, 
+                blogpost.meta['pubdate'].day,
+                blogpost.meta['slug'])
+        f = open(filename, "w")
+        f.write(blogpost.html)
+        index_file += blogpost.html
+        f.close()
+    
+    for draft in drafts:
+        filename = "%sdraft_%s.php" % (
+            settings.SERVER_ROOT,
+            draft.meta['slug']
+        )
+        f = open(filename, "w")
+        f.write(draft.html)
+        f.close()
+    
+    filename = settings.SERVER_ROOT + "main.php"
+    f = open(filename, "w")
+    f.write(index_file)
+    f.close()
+    for year in blog_index:
+        for month in blog_index[year]:
+            filename = "%s%s%02d.php" % (settings.SERVER_ROOT, year, month)
+            f = open(filename, "w")
+            blogs = sorted(blog_index[year][month], key = lambda blog: blog.meta['pubdate'])
+            for blogpost in blogs:
+                f.write(blogpost.html)
+            f.close()
+
+def get_blogposts(path):
     blogposts = []
     blog_index = {}
     for infile in glob.glob(os.path.join(
-            settings.SOURCES['published'], '*.txt')
+            path, '*.txt')
         ):
         file_obj = codecs.open(infile, mode="r", encoding="utf8")
         blog = BlogPost(file_obj)
@@ -23,32 +64,7 @@ def main():
             blog_index[year][month] = []
         blog_index[year][month].append(blog)
         file_obj.close()
-        
-    index_file = ""
-    blogs = sorted(blogposts, key=lambda blog: blog.meta['pubdate'], reverse=True)
-    for blogpost in blogs:
-        filename = "%s%s%02d%02d_%s.php" % (
-                settings.SERVER_ROOT,
-                blogpost.meta['pubdate'].year, 
-                blogpost.meta['pubdate'].month, 
-                blogpost.meta['pubdate'].day,
-                blogpost.meta['slug'])
-        f = open(filename, "w")
-        f.write(blogpost.html)
-        index_file += blogpost.html
-        f.close()
-    filename = settings.SERVER_ROOT + "main.php"
-    f = open(filename, "w")
-    f.write(index_file)
-    f.close()
-    print blog_index
-    for year in blog_index:
-        for month in blog_index[year]:
-            filename = "%s%s%02d.php" % (settings.SERVER_ROOT, year, month)
-            f = open(filename, "w")
-            blogs = sorted(blog_index[year][month], key = lambda blog: blog.meta['pubdate'])
-            for blogpost in blogs:
-                f.write(blogpost.html)
-            f.close()
+    return blogposts, blog_index
+
 if __name__ == "__main__":
     main()
--- templates/index.php
+++ templates/index.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" href="/media/css/reset.css" type="text/css" media="screen" title="no title" charset="utf-8">
+	<link rel="stylesheet" href="/media/css/taf-kickoff-m.css" type="text/css" media="screen" title="no title" charset="utf-8">
 	<link rel="stylesheet" href="/media/css/main.css" type="text/css" media="screen" title="no title" charset="utf-8">
 	<title>Bj&ouml;rn's saga</title>
 </head>
@@ -10,7 +12,16 @@
 	</div>
 	<div id="content">
 		<?php
-		if ($_GET['year'] && $_GET['month'] && $_GET['day'] && $_GET['slug']) {
+		if ($_GET['draft'] && $_GET['slug']) {
+			$filename = 'draft_' . $_GET['slug'] . '.php';
+			if (file_exists($filename)){
+				include($filename);
+			}
+			else {
+				include('404.php');
+			}
+		}
+		elseif ($_GET['year'] && $_GET['month'] && $_GET['day'] && $_GET['slug']) {
 			$filename = $_GET['year'] . $_GET['month'] . $_GET['day'] . '_' . $_GET['slug'] . '.php';
 			if (file_exists($filename)){
 				include($filename);
